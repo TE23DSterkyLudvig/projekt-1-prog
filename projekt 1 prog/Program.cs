e35623c55e1b977c8e4792e6e02e601ad3022f73
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2025-01-27 09:58:45 +0100

    fortsätter sedan

--- projekt 1 prog/Program.cs
+++ projekt 1 prog/Program.cs
@@ -1,13 +1,20 @@
-﻿int hälsa = 150;            // Hur mycket hälsa du har
+﻿using System.ComponentModel;
+
+int hälsa = 150;            // Hur mycket hälsa du har
 string val;                 // Vilket val du valt gällande rikting
-int attackVal;             //Vilken attack väljer du
-int monsterSkada = Random.Shared.Next(10, 40);           // Hur mycket skada monster gör
-int monsterHälsa = Random.Shared.Next (100, 140);               // Hur mycket hälsa ett monster kan ha
-int läka = 50;                      //Hur mycket hälsa du får vid läkning
+string attackVal = "";          //Vilken attack väljer du
+int attackValNum = 0;           // vilken nummer av attack vill du göra
+int mötandenum = 0;             //vilket monster man möter
+int monster = 0;           //vilket monsters hp
+
+int monsterSkada = Random.Shared.Next(10, 40);           // Hur mycket skada monster gör              
+int läka = 30;                      //Hur mycket hälsa du får vid läkning
 
 List<string> attacker = ["Peta", "Slå", "Klyva", "Skjuta"];               // De olika attackerna
 List<int> skada = [Random.Shared.Next(10, 20), Random.Shared.Next(20, 40), Random.Shared.Next(30, 70), Random.Shared.Next(10, 100)];           // Hur mycket skada som vapnena gör
-List<int> mötande = [Random.Shared.Next(1,3), Random.Shared.Next(1,3), Random.Shared.Next(1,3)];
+List<string> mötande = ["Giftorm", "Trädmonster", "Sork", "skogstroll"];
+List<int> monsterHälsa = [Random.Shared.Next(80, 90), Random.Shared.Next(80, 90), Random.Shared.Next(80, 90), Random.Shared.Next(80, 90)];     // Hur mycket hälsa ett monster kan ha
+
 
 System.Console.WriteLine("En dag när du är ute och vandrar i skogen så märker du plötsligt att du tappat bort dig i skogen, Du måste ta dig hem.");
 rum();
@@ -23,9 +30,24 @@ val = Console.ReadLine();
 
 val = vägVal(val);
 
-if(val == "rakt"){
 
+for (int i = 0; i < mötande.Count; i++)
+{
+ if (val == "rakt")
+{
+    
+    hälsa = läker(hälsa, läka);
 }
+else if (val == "vänster")
+{
+  möta();
+}
+else if (val == "höger")
+{
+    inget();
+}   
+}
+
 
 
 
@@ -71,12 +93,60 @@ static void rum()                      // Tidsrum mellan meningar.
     Thread.Sleep(800);
 }
 
-static int monsterStrid(int hälsa, int monsterSkada, int monsterHälsa, List<string> attacker, List<int> skada, int attackVal){
-    while(true){
+
+static void inget(){
+    System.Console.WriteLine("Inget händer.");
+}
+
+static int läker(int hälsa, int läka){
+    System.Console.WriteLine($"Du hittar en läkande flaska och läker {läka} i hälsa.");
+    hälsa += läka;
+    return hälsa;
+}
+
+
+void möta(){
+    System.Console.WriteLine($"Du möter en {mötande[mötandenum]} som kommer upp för att attackera.");
+    rum();
+    System.Console.WriteLine($"ormen har {monsterHälsa[monster]} i hälsa och gör {monsterSkada} i skada");
+    rum();
+    hälsa = monsterStrid(hälsa, monsterSkada, monsterHälsa, attacker, skada, attackVal, attackValNum, monster);
+    System.Console.WriteLine($"Du har {hälsa} kvar i hälsa");
+    monster++;
+}
+
+
+
+
+static int monsterStrid(int hälsa, int monsterSkada, List<int> monsterHälsa, List<string> attacker, List<int> skada, string attackVal, int attackValNum, int monster)
+{
+    while (true)
+    {
+        System.Console.WriteLine("Vilken attack vill du använda? Skriv 1 nummer 1 till 4.");
+        attackVal = Console.ReadLine();
+        int.TryParse(attackVal, out attackValNum);
+        attackValNum--;
+
+
         hälsa -= monsterSkada;
-        monsterHälsa -= skada[attackVal];
-        System.Console.WriteLine($"Monstret har {monsterHälsa} i hälsa kvar");
+        System.Console.WriteLine($"Monster gör {monsterSkada} i skada på dig");
+        Thread.Sleep(800);
+        monsterHälsa[monster] -= skada[attackValNum];
+        System.Console.WriteLine($"Du skadar monstret med {attacker[attackValNum]} och gör {skada[attackValNum]} på monstret");
+        Thread.Sleep(800);
+        System.Console.WriteLine($"Monstret har {monsterHälsa[monster]} i hälsa kvar");
+        Thread.Sleep(800);
         System.Console.WriteLine($"Du har {hälsa} i hälsa kvar");
-        if 
+        Thread.Sleep(800);
+        if (monsterHälsa[monster] <= 0)
+        {
+            System.Console.WriteLine("Du Dödade monstret");
+            return hälsa;
+        }
+        else if (hälsa <= 0)
+        {
+            System.Console.WriteLine("Du är död");
+            return hälsa;
+        }
     }
 }
